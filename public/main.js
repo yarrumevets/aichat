ddcecc2ca7645832366609ade820ca95b7e7aa5a
Steve M <steve@yarrumevets.com>
2024-02-04 15:04:52 -0800

    updated server port.

---
+++ public/main.js
@@ -0,0 +1,83 @@
+const sendButton = document.getElementById("sendbutton");
+const inputField = document.getElementById("chatinput");
+const chatLog = document.getElementById("chatlog");
+
+const createChatLog = (username, message) => {
+  const newLog = document.createElement("p");
+  const logName = document.createElement("span");
+  const logMessage = document.createElement("span");
+  const msgCost = document.createElement("span");
+  newLog.classList.add("log");
+  logName.classList.add("logname");
+  logMessage.classList.add("logmessage");
+  msgCost.classList.add("msgcost");
+  logName.innerHTML = username;
+  logMessage.innerHTML = message;
+  newLog.append(msgCost);
+  newLog.append(logName);
+  newLog.append(logMessage);
+  chatLog.append(newLog);
+  return msgCost;
+};
+
+const calcCost = (tokens, type) => {
+  const costs = {
+    prompt: 0.0000015, // 0.0015 / 1000,
+    completion: 0.000002, // 0.002 / 1000,
+  };
+  return tokens * costs[type];
+};
+
+async function sendMessage(message) {
+  const myMsgCost = createChatLog("You", message);
+  const messageData = {
+    message,
+  };
+  const response = await fetch("/api/send", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(messageData),
+  });
+  const jsonResponse = await response.json();
+  console.log("Message status: ", jsonResponse.response);
+
+  const chatGPTMsgCost = createChatLog("ChatGPT", jsonResponse.response);
+
+  const promptCost = calcCost(jsonResponse.usage.prompt_tokens, "prompt");
+  const completionCost = calcCost(
+    jsonResponse.usage.completion_tokens,
+    "completion"
+  );
+
+  //const pCents = promptCost / 100;
+  //const cCents = completionCost / 100;
+
+  // console.log("cost ", promptCost, " / 100 = ", pCents);
+
+  // myMsgCost.innerHTML = `&#162;${pCents}`;
+  // chatGPTMsgCost.innerHTML = `&#162;${cCents}`;
+
+  myMsgCost.innerHTML = `$${promptCost.toFixed(7)}`;
+  chatGPTMsgCost.innerHTML = `$${completionCost.toFixed(7)}`;
+}
+
+const doSend = () => {
+  const message = inputField.value;
+  if (!message) return;
+  inputField.value = "";
+  console.log(Date.now(), " M: ", message);
+  inputField.focus();
+  sendMessage(message);
+};
+
+inputField.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    doSend();
+  }
+});
+
+sendButton.onclick = () => {
+  doSend();
+};
---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4321;
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile("public/index.html", { root: __dirname });
+});
+
+const openaiApiCreds = require("./openaiApiCreds.json");
+
+app.post("/api/send", (req, res) => {
+  console.log("----------------------[ MESSAGE ]----------------------");
+  const message = req.body.message;
+  console.log("Message from user: ", message);
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", `Bearer ${openaiApiCreds.openaiApiKey}`);
+
+  const raw = JSON.stringify({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are Hunter S. Thompson. The American writer famous for 'Fear and Loathing in Las Vegas' and 'The Rum Diary', among others. Give me a short 2 or 3 sentence answer in your distinctive writing style.",
+      },
+      {
+        role: "user",
+        content: message,
+      },
+    ],
+    temperature: 0.7,
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  const startFetchTime = process.hrtime();
+  console.log("Getting GPT reply...");
+
+  fetch("https://api.openai.com/v1/chat/completions", requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      jsonResult = JSON.parse(result);
+      if (jsonResult && jsonResult.choices && jsonResult.choices[0]) {
+        const responseMessage = jsonResult.choices[0].message.content;
+
+        // Display the time it took to receive a response from GPT API:
+        const end = process.hrtime(); // Record the end time
+        const durationInNanoseconds =
+          end[0] * 1e9 + end[1] - (startFetchTime[0] * 1e9 + startFetchTime[1]);
+        const durationInSeconds = durationInNanoseconds / 1e9; // Convert to seconds
+        console.log(`...received in ${durationInSeconds} seconds.`);
+
+        res.send({ response: responseMessage, usage: jsonResult.usage });
+      } else {
+        res.send({ response: "" });
+      }
+    })
+    .catch((error) => console.log("error", error));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
